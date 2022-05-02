45c20283fb2e5708957d3761fb170de63ce3ae7a
snehalyutika <103288241+snehalyutika@users.noreply.github.com>
2022-05-01 23:16:46 -0700

    created a test file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,6 @@
 public class SkillDemo {
 
+    public static int avg(int n, int m){
+        return (n+m)/2;
+    }
 }
---
+++ SkillDemoTestfile.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTestfile {
+    @Test
+    public void addition() {
+        assertEquals(2,SkillDemo.avg(1,1));
+    } 
+}
